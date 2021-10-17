a19c2447b627eaaa9a20f49087ce422b8d5f3bac
Roman Matveyenko <matveyenkaraman@gmail.com>
2021-10-17 16:27:56 +0300

    first commit

---
+++ nest-one-array-within-another-array.js
@@ -0,0 +1,2 @@
+// Only change code below this line
+var myArray = [['Roman', 17],['Roman', 37]];
---
+++ shopping-list.js
@@ -0,0 +1,7 @@
+var myList = [
+  ['bread', 1],
+  ['meat', 3],
+  ['milk', 1],
+  ['cream', 3],
+  ['cottage cheas', 5],
+  ];
